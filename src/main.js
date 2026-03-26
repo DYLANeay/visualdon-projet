31a8d6e8b81bf5b1164e915a7f8e3f70419ec168
Loïc Peyramaure <loic63130@gmail.comm>
2026-03-26 11:46:06 +0100

    modification de elections.json (evenements enlevé), ajout fichier API.js avec un parseur, import des data de elections EU + NoPasaran

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
+import { loadJSON } from './api.js'
+
+const dataEuElections = await loadJSON('../data/elections/elections.js')
+const dataNoPasaran = await loadJSON('../data/nopasaran/data.json')
+
 console.log("hello");
